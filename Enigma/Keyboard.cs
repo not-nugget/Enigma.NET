6ab339bfa7d1c4f20c01f4086c304d9485b402f8
not-nugget <19211255+not-nugget@users.noreply.github.com>
2025-07-06 12:29:28 -0400

    initial Keyboard impl
    there will be some improvements to make, such as inputting strings/char spans/streams/etc.

---
+++ Enigma/Keyboard.cs
@@ -0,0 +1,19 @@
+﻿namespace Enigma;
+
+/// <summary>Enters the Enigma system by converting various inputs into their respective <see cref="Letter"/> outputs</summary>
+public readonly struct Keyboard()
+{
+    private readonly Letter[] _keys = Letter.Cache;
+
+    /// <summary>Converts <paramref name="key"/> into its <see cref="Letter"/> counterpart, or returns <see cref="Letter.Invalid"/></summary>
+    public Letter Press(char key)
+    {
+        foreach (var letter in _keys.AsSpan())
+        {
+            if (key == letter.Upper || key == letter.Lower)
+                return letter;
+        }
+
+        return Letter.Invalid;
+    }
+}
